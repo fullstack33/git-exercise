2baa6666d4f66ad4e1cce14f4469fe3635561adb
Nawal2733 <34098265+Nawal2733@users.noreply.github.com>
2020-03-12 10:09:40 +0530

    Update matrix.js

--- JavaScript_assisment_1/Exam2020/public/js/matrix.js
+++ JavaScript_assisment_1/Exam2020/public/js/matrix.js
@@ -26,24 +26,24 @@ function mapdata() {
         }
       })
     }
-    // if (j - 1 >= 0 && i + 1 < row) {
-    //   edges.push({
-    //     "data": {
-    //       "id": `${i}-${j}WN`,
-    //       source: `N${i}-${j}`,
-    //       target: `N${i+1}-${j-1}`
-    //     }
-    //   })
-    // }
-    // if (j - 1 >= 0 && i - 1 >= 0) {
-    //   edges.push({
-    //     "data": {
-    //       "id": `${i}-${j}WS`,
-    //       source: `N${i}-${j}`,
-    //       target: `N${i-1}-${j-1}`
-    //     }
-    //   })
-    // }
+    if (j - 1 >= 0 && i + 1 < row) {
+      edges.push({
+        "data": {
+          "id": `${i}-${j}WN`,
+          source: `N${i}-${j}`,
+          target: `N${i+1}-${j-1}`
+        }
+      })
+    }
+    if (j - 1 >= 0 && i - 1 >= 0) {
+      edges.push({
+        "data": {
+          "id": `${i}-${j}WS`,
+          source: `N${i}-${j}`,
+          target: `N${i-1}-${j-1}`
+        }
+      })
+    }
 
   }
 
